020891761d17b924cfa90724e30291aa46944d88
Irmak Erdem <irmakerdem9@gmail.com>
2022-07-28 20:23:11 -0600

    Add details functionality to DetailsContainer

--- src/components/DetailsContainer/DetailsContainer.js
+++ src/components/DetailsContainer/DetailsContainer.js
@@ -14,19 +14,30 @@ const DetailsContainer = ({selectedTree, clearSelectedTree}) => {
     return (
        <>
        <h2>{`About: ${selectedTree.tree_name}`}</h2>
-       <h3>{`${selectedTree.tree_name} Zone: ${selectedTree.growing_zone}`}</h3>
-       <section>
-            {/* IMAGES GO HERE */}
+       <p>{`Botanical Name: ${selectedTree.botanical_name}`}</p><br></br>
+       <h3>{`${selectedTree.tree_name} Growing Zone: ${selectedTree.growing_zone}`}</h3>
+       <section className='detailImages'>
+         <img src={selectedTree.main_tree_image} alt={selectedTree.tree_name} /><br></br>
+         <img src={selectedTree.tree_image_1} alt={selectedTree.tree_name} /><br></br>
+         <img src={selectedTree.tree_image_2} alt={selectedTree.tree_name} /><br></br>
+         <img src={selectedTree.tree_image_3} alt={selectedTree.tree_name} /><br></br>
+         <img src={selectedTree.tree_image_4} alt={selectedTree.tree_name} /><br></br>
       </section>
       <section>
-      <p>Attributes</p>
+      <p>📝 Attributes</p>
         <p dangerouslySetInnerHTML={ createHTML() } />
        </section>
        <section>
-        <p>Sun</p>
-        <p>Soil</p>
-        <p>Growth Speed</p>
-        <p>Height</p>
+        <p>{`☀️ Sun Preference: ${selectedTree.sun_preference}`}</p><br></br>
+        <p>{`🪴 Soil Preference: ${selectedTree.soil_preference}`}</p><br></br>
+        <p>{`🐢 Growth Speed: ${selectedTree.growth_speed}`}</p><br></br>
+        <p>{`📏 Mature Height: ${selectedTree.mature_height}`}</p><br></br>
+        <p>{`🌳 Mature Spread: ${selectedTree.mature_speed}`}</p><br></br>
+        <p>{`🌺 Flower Color: ${selectedTree.flower_color}`}</p><br></br>
+        <p>{`🍃 Foliage Color: ${selectedTree.foliage_color}`}</p><br></br>
+        <p>{`🍂 Fall Color: ${selectedTree.fall_color}`}</p><br></br>
+        <p>{`🐝 Pollinator Friendly: ${selectedTree.pollinator_friendly}`}</p><br></br>
+        <a href={selectedTree.outgoing_link}>Find more info at Nature Hills</a><br></br>
        </section>
       <Link to='/result'>
          <img src={arrow} alt='black arrow pointing to the left' onClick={() => clearSelectedTree()}></img>
