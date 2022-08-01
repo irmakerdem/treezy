803c439b1c2dd735bc22aa4c425a1731a6fd4912
Baskanbetul <btlbskn@hotmail.com>
2022-08-01 11:50:24 -0600

    Add mock data files for tree and zip

--- cypress/e2e/Home.cy.js
+++ cypress/e2e/Home.cy.js
@@ -1,5 +1,10 @@
-describe('empty spec', () => {
-  it('passes', () => {
+describe('Home', () => {
+  beforeEach('The user should see the home page', () => {
+    cy.intercept('GET', 'https://tree-pal-api.herokuapp.com/api/v1/trees', {fixture : 'getTreeMockData.json'})
+    cy.intercept('GET', 'https://tree-pal-api.herokuapp.com/api/v1/zip', {fixture : 'getZipMockData.json'})
+    cy.visit('https://localhost:3000/');
+  })
+  it('Should ', () => {
     cy.visit('https://example.cypress.io')
   })
 })
