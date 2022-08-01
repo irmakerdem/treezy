37f8062fdcd081066acd4ff0d4823a068b9fa2b0
Baskanbetul <btlbskn@hotmail.com>
2022-08-01 12:05:13 -0600

    Add beforeEach, fixtures and initial it block

--- cypress/e2e/Home.cy.js
+++ cypress/e2e/Home.cy.js
@@ -1,10 +1,12 @@
 describe('Home', () => {
-  beforeEach('The user should see the home page', () => {
-    cy.intercept('GET', 'https://tree-pal-api.herokuapp.com/api/v1/trees', {fixture : 'getTreeMockData.json'})
-    cy.intercept('GET', 'https://tree-pal-api.herokuapp.com/api/v1/zip', {fixture : 'getZipMockData.json'})
-    cy.visit('https://localhost:3000/');
+  beforeEach(() => {
+    cy.intercept('GET', 'https://tree-pal-api.herokuapp.com/api/v1/trees', {fixture : 'getTreeMockData.json'});
+    cy.intercept('GET', 'https://tree-pal-api.herokuapp.com/api/v1/zip', {fixture : 'getZipMockData.json'});
+    cy.visit('http://localhost:3000/');
   })
-  it('Should ', () => {
-    cy.visit('https://example.cypress.io')
+
+  it('The user should see the home page', () => {
+    cy.get('[data-cy="header"]').contains('Treezy');
   })
+
 })
