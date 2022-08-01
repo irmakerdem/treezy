dc81e06c49480bd1765e7738652f724d51d7ff1e
Baskanbetul <btlbskn@hotmail.com>
2022-08-01 17:04:51 -0600

    Add testing for checking result-top, tree-name and tree-image on searchResult file

--- cypress/e2e/SearchResult.cy.js
+++ cypress/e2e/SearchResult.cy.js
@@ -1,5 +1,21 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Search Result', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://tree-pal-api.herokuapp.com/api/v1/trees', {fixture : 'getTreeMockData.json'});
+    cy.intercept('GET', 'https://tree-pal-api.herokuapp.com/api/v1/zip', {fixture : 'getZipMockData.json'});
+    cy.visit('http://localhost:3000/');
   })
+
+  it('The user should see the home page', () => {
+    cy.get('[data-cy="zip-code-entry"]').type('80101');
+    cy.get('[data-cy="go-button"]').invoke('attr', 'value').should('contain', 'GO!')
+    cy.get('[data-cy="go-button"]').click()
+    cy.url().should('eq', 'http://localhost:3000/result')
+    cy.get('.results-top').contains('AGATE')
+    cy.get('.tree-name').contains('Jane Magnolia')
+    cy.get('.trees-container').within(() => {
+    cy.get('.tree-image').should('have.length', 1);
+    cy.get('.tree-image').should('have.attr', 'src', 'https://raw.githubusercontent.com/irmakerdem/tree-pal-api/main/assets/jane-magnolia/jane-magnolia-main-wo-border.jpg')  
+  })
+ })
 })
+
